efd34b9309033a727ae5fc7e10223403c3ace57e
rajkumarpathak879 <rajkumarpathak879@gmail.com>
2021-01-09 15:34:30 +0530

    practice javascript

---
+++ javascript30/Countdown Clock/scripts-START.js
@@ -0,0 +1,54 @@
+let coundown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+function timer(seconds){
+    //clear any existing timer
+    clearInterval(coundown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds)
+    displayEndTime(then)
+
+    coundown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if(secondsLeft <=0){
+            clearInterval(coundown);
+            return;
+        }
+
+        //console.log(secondsLeft); 
+        displayTimeLeft(secondsLeft)
+    }, 1000);
+}
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}: ${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+    //console.log({minutes,remainderSeconds});
+    //console.log(seconds);
+}
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    const adjustHours = hour > 12 ? hour - 12 : hour;
+    endTime.textContent = `Be Back At ${adjustHours}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer(){
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click',startTimer));
+document.customForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+});
---
+++ javascript30/Webcam Fun/scripts.js
@@ -0,0 +1,44 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video:true,audio:false})
+    .then(localMediaStream =>{
+        console.log(localMediaStream);
+        //video.src = window.URL.createObjectURL(localMediaStream);
+        video.srcObject=localMediaStream;
+        video.play();
+    })
+    .catch(err =>{
+        console.error('OHH No!!');
+    });
+}
+
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(()=>{
+        ctx.drawImage(video,0,0,width,height);
+    },16);
+}
+
+function takePhoto(){
+    snap.currentTime = 0;
+    snap.play();
+    //take data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    //console.log(data);
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download','handsome');
+    link.textContent ='Download Image';
+    strip.insertBefore(link,strip.firstChild);
+}
+getVideo();
+video.addEventListener('canplay',paintToCanvas);
